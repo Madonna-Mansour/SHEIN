737da0e07d07086cba1b7f0d405e72779e6d0de1
Madonna-Mansour <madonnamansour57@gmail.com>
2025-05-28 14:52:10 +0300

    search

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { TbWorld } from "react-icons/tb";
 import { RiCustomerService2Fill } from "react-icons/ri";
@@ -8,13 +8,14 @@ import { IoPersonOutline } from "react-icons/io5";
 import { IoIosSearch } from "react-icons/io";
 import { IoIosLogOut } from "react-icons/io";
 import { useDispatch, useSelector } from 'react-redux';
-import { logOut, setSearchTerm } from '../redux/appSlice'; // ✅ تم التعديل هنا
+import { logOut, setSearchTerm } from '../redux/appSlice';
 import { getAuth, signOut } from "firebase/auth";
 
 const Nav = () => {
   const user = useSelector((state) => state.appReducer.userInfo);
-  const searchTerm = useSelector((state) => state.appReducer.searchTerm);
   const dispatch = useDispatch();
+
+  const [input, setInput] = useState(''); // ✅ حالة محلية لتخزين الإدخال
 
   const handleLogOut = () => {
     const auth = getAuth();
@@ -25,20 +26,27 @@ const Nav = () => {
     });
   };
 
+  const handleSearch = () => {
+    dispatch(setSearchTerm(input)); // ✅ فقط عند الضغط على الزر
+  };
+
   return (
-    <div className='fixed top-0 w-[100%] bg-white flex justify-between items-center p-4 font-bold border border-b'>
+    <div className='fixed top-0 w-full bg-white flex justify-between items-center p-4 font-bold border border-b z-50'>
       <div>
         <h1 className='text-[35px] xs:text-[20px] md:text-[35px]'>SHEIN</h1>
       </div>
 
-      <div className="flex ">
+      <div className="flex">
         <input
-          value={searchTerm}
-          onChange={(e) => dispatch(setSearchTerm(e.target.value))}
-          className='w-[400px] h-10 p-4 border border-black  xs:w-[90px] md:w-[400px] rounded-none'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className='w-[400px] h-10 p-4 border border-black xs:w-[90px] md:w-[400px] rounded-none'
           type="text"
         />
-        <button className='w-[40px]  text-white flex justify-center items-center text-[27px] h-10 bg-black'>
+        <button
+          className='w-[40px] text-white flex justify-center items-center text-[27px] h-10 bg-black'
+          onClick={handleSearch}
+        >
           <IoIosSearch />
         </button>
       </div>
@@ -52,7 +60,7 @@ const Nav = () => {
           )}
         </div>
         <div className="mx-4">
-          <Link  to={"/cart"}><SlBasket /></Link>
+          <Link to="/cart"><SlBasket /></Link>
         </div>
         {user && (
           <div>
@@ -60,7 +68,7 @@ const Nav = () => {
           </div>
         )}
         <div>
-          <Link to={"/favorit"}><MdFavoriteBorder /></Link>
+          <Link to="/favorit"><MdFavoriteBorder /></Link>
         </div>
         <div className="mx-4 hidden md:flex">
           <RiCustomerService2Fill />
