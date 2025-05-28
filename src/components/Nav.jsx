3997f5822125f7b9f40997465f3b8e2910786dcc
Madonna-Mansour <madonnamansour57@gmail.com>
2025-05-28 15:13:37 +0300

    search

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,8 +5,7 @@ import { RiCustomerService2Fill } from "react-icons/ri";
 import { MdFavoriteBorder } from "react-icons/md";
 import { SlBasket } from "react-icons/sl";
 import { IoPersonOutline } from "react-icons/io5";
-import { IoIosSearch } from "react-icons/io";
-import { IoIosLogOut } from "react-icons/io";
+import { IoIosSearch, IoIosLogOut } from "react-icons/io";
 import { useDispatch, useSelector } from 'react-redux';
 import { logOut, setSearchTerm } from '../redux/appSlice';
 import { getAuth, signOut } from "firebase/auth";
@@ -15,19 +14,21 @@ const Nav = () => {
   const user = useSelector((state) => state.appReducer.userInfo);
   const dispatch = useDispatch();
 
-  const [input, setInput] = useState(''); // ✅ حالة محلية لتخزين الإدخال
+  const [input, setInput] = useState('');
 
   const handleLogOut = () => {
     const auth = getAuth();
-    signOut(auth).then(() => {
-      dispatch(logOut());
-    }).catch((error) => {
-      console.log(error);
-    });
+    signOut(auth)
+      .then(() => {
+        dispatch(logOut());
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   const handleSearch = () => {
-    dispatch(setSearchTerm(input)); // ✅ فقط عند الضغط على الزر
+    dispatch(setSearchTerm(input.trim())); // إذا كانت فاضية، يرسل قيمة فاضية تلقائيًا
   };
 
   return (
@@ -40,8 +41,12 @@ const Nav = () => {
         <input
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') handleSearch();
+          }}
           className='w-[400px] h-10 p-4 border border-black xs:w-[90px] md:w-[400px] rounded-none'
           type="text"
+          placeholder="ابحث عن منتج..."
         />
         <button
           className='w-[40px] text-white flex justify-center items-center text-[27px] h-10 bg-black'
